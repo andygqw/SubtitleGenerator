7a2b3f08f87171ac178812ed410095b45a2ff960
andygqw <andygqwang@gmail.com>
2024-08-27 02:31:22 -0700

    Extractor

---
+++ src/main/java/org/example/Extractor.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+
+public class Extractor implements Runnable {
+
+    Iterator<Path> iterator;
+
+    String inputAudioPath;
+    String outputSrtPath;
+
+    public Extractor(String path){
+
+        List<Path> files = new LinkedList<>();
+
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            paths.filter(Files::isRegularFile)
+                    .filter(filePath -> !filePath.getFileName().toString().equals(".DS_Store"))
+                    .filter(filePath -> getFileExtension(filePath.getFileName().toString()).equals("mp3"))
+                    .forEach(files::add);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        iterator = files.iterator();
+    }
+
+    /**
+     * Get the extension of the given file path
+     *
+     * @param filePath The file path
+     * @return the extension
+     * */
+    public static String getFileExtension(String filePath) {
+        int dotIndex = filePath.lastIndexOf('.');
+
+        if (dotIndex > 0 && dotIndex < filePath.length() - 1) {
+            return filePath.substring(dotIndex + 1);
+        }
+        return "";
+    }
+
+    /**
+     * Change the file extension of the given file path.
+     *
+     * @param filePath The original file path
+     * @return The file path with the new extension
+     */
+    private static String changeFileExtension(String filePath) {
+
+        String newExtension = "srt";
+
+        // Find the last dot in the file path
+        int dotIndex = filePath.lastIndexOf('.');
+
+        // If there's no dot or it's in the first position, we assume there's no extension
+        if (dotIndex == -1 || dotIndex == 0) {
+
+            return filePath + "." + newExtension;
+        }
+
+        return filePath.substring(0, dotIndex + 1) + newExtension;
+    }
+
+    public boolean setNext(){
+
+        if (iterator.hasNext()) {
+
+            inputAudioPath = iterator.next().toString();
+            outputSrtPath = changeFileExtension(inputAudioPath);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Extractor: " + inputAudioPath + " -> " + outputSrtPath);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        final int MAX_SIZE = 4;
+        PriorityQueue<Runnable> queue = new PriorityQueue<>();
+
+
+        Extractor extractor = new Extractor("/Volumes/Andys_SSD/Online_Courses/Antra");
+
+        // Producer Thread
+        new Thread(() -> {
+
+            while (true) {
+                synchronized (queue) {
+                    while (queue.size() == MAX_SIZE) {
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+
+        }).start();
+
+
     }
+
 }
